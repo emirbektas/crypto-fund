29506fccca027cea1b64cd2b72c6c22bf60ca04b
emirbkts <emr.bkts@hotmail.com>
2023-07-16 17:45:06 +0300

    frameworks

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <div className="container">
+        <h1 className="text-xl text-red-500">deneme</h1>
+        <h2 className="text-green-900 text-5xl">42</h2>
+        <ul className="flex gap-10 justify-center">
+          <li>asd</li>
+          <li>asd</li>
+          <li>asd</li>
+        </ul>
+      </div>
+    </main>
+  );
+}
