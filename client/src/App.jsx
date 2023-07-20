5a175faee445a92921f7f0288e80da63c47ef6b4
emirbkts <emr.bkts@hotmail.com>
2023-07-20 17:34:39 +0300

    react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
+import { Sidebar, Navbar } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="relative sm:-8 p-4 bg-[#131318] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        Navbar
+    <BrowserRouter>
+      <div className="relative sm:-8 p-4 bg-[#131318] min-h-screen flex flex-row">
+        <div className="sm:flex hidden mr-10 relative">
+          <Sidebar />
+        </div>
+        <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
