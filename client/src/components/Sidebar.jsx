85ec368e1fa6569cfa5c078775a8f1ba331fb063
emirbkts <emr.bkts@hotmail.com>
2023-08-17 11:36:41 +0300

    bug fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { logo, sun } from "../assets";
+import { logo, sun, thirdweb } from "../assets";
 import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
@@ -34,7 +34,7 @@ function Sidebar() {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={thirdweb} />
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12 ">
